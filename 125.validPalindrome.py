f6c0e09a3a0f5383aa640d4fe31a2fd6eca4c907
changhunJung <changhun611@gmail.com>
2022-01-24 23:42:13 +0900

    s.lower() and re.sub() returns str. It doesn't modify the input itself.

--- 125.validPalindrome.py
+++ 125.validPalindrome.py
@@ -6,7 +6,9 @@ class Solution:
 
     def isPalindrome(self, s: str) -> bool:
         low = s.lower()
-        re.sub('[^a-z0-9]', '', low)
+        print(low)
+        low = re.sub('[^a-z0-9]', '', low)
+        print(low)
         return low == low[::-1]
 
 s = "A man, a plan, a canal: Panama"
