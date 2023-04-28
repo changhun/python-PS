32683adaa7c76ac5eaf65c9fb22c2aff39c68646
changhunJung <changhun611@gmail.com>
2023-04-28 23:47:36 +0900

    .

---
+++ 파이썬알고리즘인터뷰/81. 주유소 (leetcode134).py
@@ -0,0 +1,19 @@
+from typing import List
+
+
+class Solution:
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+
+        if sum(gas) < sum(cost):
+            return -1
+
+        start = fuel = 0
+        for i in range(len(gas)):
+            fuel += gas[i] - cost[i]
+            if fuel < 0:
+                fuel = 0
+                start = i+1
+
+        if start == len(gas):
+            return -1
+        return start
