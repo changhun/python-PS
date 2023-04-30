7cd94ca96dfb48afee417104a07b9535981f8585
changhunJung <changhun611@gmail.com>
2023-04-30 23:12:30 +0900

    .

---
+++ 파이썬알고리즘인터뷰/68. 두 수의 합2 (leetcode 167).py
@@ -0,0 +1,26 @@
+from typing import List
+import bisect
+
+"""
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        for i in range(len(numbers) - 1):
+            ret = bisect.bisect_left(numbers[i+1:], target - numbers[i])
+            if i + 1 + ret < len(numbers) and numbers[i + 1 + ret] == target - numbers[i]:
+                return [i + 1, i + ret + 1 + 1]
+"""
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        for i in range(len(numbers) - 1):
+            ret = bisect.bisect_left(numbers, target - numbers[i], i + 1)
+            if ret < len(numbers) and numbers[ret] == target - numbers[i]:
+                return [i + 1, ret + 1]
+
+
+
+numbers = [2,7,11,15]
+target = 9
+numbers = [5,25,75]
+target = 100
+ret = Solution().twoSum(numbers, target)
+print(ret)
