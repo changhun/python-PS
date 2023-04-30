5d90956beb2a0cf277f8bf44a1a2d697a82d00df
changhunJung <changhun611@gmail.com>
2023-04-30 16:13:20 +0900

    .

---
+++ 파이썬알고리즘인터뷰/60. 삽입 정렬 리스트.py
@@ -0,0 +1,36 @@
+from typing import Optional
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def insertionSortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head:
+            return None
+
+        dummy = ListNode(0, head)
+        cur = head.next
+        last = head
+        last.next = None # this is the key point which makes the end sorted list is null
+
+        while cur:
+            next = cur.next
+            prev = dummy
+            #while prev.next and prev.next is not cur and prev.next.val <= cur.val:
+            while prev.next and prev.next.val <= cur.val:
+                prev = prev.next
+
+            # if cur should be positioned at the last node of previous sorted list
+            #if not prev.next:
+            #    cur.next = None # this is done in the following line. (cur.next = prev.next)
+
+            cur.next, prev.next = prev.next, cur
+            cur = next
+
+        head = dummy.next
+        del dummy
+        return head
