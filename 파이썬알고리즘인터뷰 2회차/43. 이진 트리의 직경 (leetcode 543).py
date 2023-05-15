56ee1949ad9a7646ec1fa67907103f631180f114
changhunJung <changhun611@gmail.com>
2023-05-15 23:30:17 +0900

    .

---
+++ 파이썬알고리즘인터뷰 2회차/43. 이진 트리의 직경 (leetcode 543).py
@@ -0,0 +1,27 @@
+from typing import Optional
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    ans = 0
+    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        def max_depth(root):
+            if not root:
+                return 0
+
+            left_depth = max_depth(root.left)
+            right_depth = max_depth(root.right)
+
+            self.ans = max(self.ans, left_depth + right_depth)
+            return max(left_depth, right_depth) + 1
+
+        max_depth(root)
+        return self.ans
+
+
