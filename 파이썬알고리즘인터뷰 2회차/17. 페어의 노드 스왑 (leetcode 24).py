24d0527bb5d82a51441394f5a4b8c87cbff98cfd
changhunJung <changhun611@gmail.com>
2023-05-18 23:08:28 +0900

    .

---
+++ 파이썬알고리즘인터뷰 2회차/17. 페어의 노드 스왑 (leetcode 24).py
@@ -0,0 +1,15 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def swapPairs(self, head: ListNode) -> ListNode:
+        if not head or not head.next:
+            return head
+
+        next = head.next
+        head.next, next.next = self.swapPairs(next.next), head
+        return next
