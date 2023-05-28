589d70e746659a67456b516c4b0e1397436fe68d
changhunJung <changhun611@gmail.com>
2023-05-28 21:43:10 +0900

    .

---
+++ 파이썬알고리즘인터뷰 2회차/64. 원점에 K 번재로 가까운 점 (leetcode 973).py
@@ -0,0 +1,7 @@
+from typing import List
+
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        points.sort(key=lambda x : x[0]**2 + x[1]**2)
+        ans = [point for point in points[:k]]
+        return ans
