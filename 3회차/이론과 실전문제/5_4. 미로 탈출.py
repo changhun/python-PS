43966f943974efcd0c31160a4fc4311f9944ba51
changhunJung <changhun611@gmail.com>
2023-06-18 23:51:22 +0900

    .

---
+++ 3회차/이론과 실전문제/5_4. 미로 탈출.py
@@ -0,0 +1,36 @@
+import collections
+#N, M = map(int, input().split())
+#print(N, M)
+n, m = map(int, input().split())
+print(n, m)
+board_str = []
+for _ in range(N):
+    board_str.append(input())
+
+board = []
+for i, low in enumerate(board_str):
+    for char in low:
+        board[i].append(int(char))
+
+
+move = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+def bfs(graph, y, x):
+    q = collections.deque()
+    q.append((y, x, 1))
+    board[y][x] = 0
+    while q:
+        y, x, dist = q.popleft()
+        if y == N - 1 and x == M - 1:
+            return dist
+
+        for m in move:
+            ny = y + m[0]
+            nx = x + m[1]
+            if 0 <= ny <= N - 1 and 0 <= nx <= M - 1 and board[ny][nx] == 1:
+                board[ny][nx] = 0
+                q.append((ny, nx, dist + 1))
+
+    return -1
+
+ans = bfs(board, 0, 0)
+print(ans)
