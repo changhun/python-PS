e43bdaba585423a9d9f6286aee041be91d89fe8c
changhunJung <changhun611@gmail.com>
2023-06-21 23:49:28 +0900

    .

---
+++ 파이썬알고리즘인터뷰 3회차/07. 두 수의 합 (leetcode 01).py
@@ -0,0 +1,18 @@
+from typing import List
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        dic = {}
+        for i, num in enumerate(nums):
+            if target - num in dic:
+                return [i, dic[target-num]]
+            dic[num] = i
+
+        return []
+
+nums = [2,7,11,15]
+target = 9
+nums = [3,2,4]
+target = 6
+ret = Solution().twoSum(nums, target)
+print(ret)
