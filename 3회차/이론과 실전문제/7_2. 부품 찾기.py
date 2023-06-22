0b9425512e4d4af22b50f17fa444793c20316e9c
changhunJung <changhun611@gmail.com>
2023-06-23 00:00:06 +0900

    .

--- 3회차/이론과 실전문제/7_2. 부품 찾기.py
+++ 3회차/이론과 실전문제/7_2. 부품 찾기.py
@@ -8,19 +8,27 @@ l2 = list(map(int, input().split()))
 l1.sort()
 
 for val in l2:
+    """
     left = 0
     right = len(l1) - 1
-    while left < right:
+    ans = False
+    while left <= right:
         mid = (left + right) // 2
         if l1[mid] == val:
+            ans = True
             break
         elif l1[mid] < val:
-            left = mid
+            left = mid + 1
         else:
-            right = mid
-    if l1[mid] == val:
+            right = mid - 1
+    if ans:
+        print("yes")
+    else:
+        print("no")
+    """
+    pos = bisect.bisect_left(l1, val)
+    if pos < len(l1) and l1[pos] == val:
         print("yes")
     else:
         print("no")
 
-    #pos = bisect.bisect_left()
