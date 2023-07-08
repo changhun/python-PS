dd1fa36990d05c48de414a7324ac9852db34aef6
changhunJung <changhun611@gmail.com>
2023-07-08 09:18:42 +0900

    .

--- 3회차/이론과 실전문제/13_15. 특정 거리의 도시 찾기.py
+++ 3회차/이론과 실전문제/13_15. 특정 거리의 도시 찾기.py
@@ -1,29 +1,43 @@
 import collections
+import sys
+input = sys.stdin.readline
+
 INF = int(1e9)
 n, m, k, x = map(int, input().split())
-graph = collections.defaultdict(list)
+
+#graph = collections.defaultdict(list)
+graph = [[] for _ in range(n+1)]
+
 for _ in range(m):
     a, b = map(int, input().split())
     graph[a].append(b)
 
-ans = 0
+ans = []
 dist = [INF]*(n+1)
 dist[x] = 0
 q = collections.deque()
 q.append(x)
 while q:
     here = q.popleft()
+    """
     if dist[here] > k:
         break
     if dist[here] == k:
-        ans += 1
+        ans.append(here)
+    """
 
     for there in graph[here]:
         if dist[there] == INF:
             dist[there] = dist[here] + 1
             q.append(there)
 
-if ans == 0:
+for i, val in enumerate(dist):
+    if val == k:
+        ans.append(i)
+
+#if not ans:
+if len(ans) == 0:
     print(-1)
 else:
-    print(ans)
+    for val in ans:
+        print(val)
