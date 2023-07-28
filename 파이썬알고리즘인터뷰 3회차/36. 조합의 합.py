eeb4144c08ee585de15d249c3a9907088642db91
changhunJung <changhun611@gmail.com>
2023-07-28 23:41:19 +0900

    .

---
+++ 파이썬알고리즘인터뷰 3회차/36. 조합의 합.py
@@ -0,0 +1,22 @@
+class Solution:
+    def combinationSum(self, candi: list[int], target: int) -> list[list[int]]:
+        def dfs(si: int, sub_target: int, comb: list[int]):
+            if sub_target == 0:
+                return ans.append(comb[:])
+            if sub_target < 0:
+                return
+            ret = 0
+            for i in range(si, len(candi)):
+                comb.append(candi[i])
+                dfs(i, sub_target - candi[i], comb)
+                comb.pop()
+
+        ans = []
+
+        dfs(0, target, [])
+        return ans
+
+candidates =[2,3,6,7]
+target = 7
+ret = Solution().combinationSum(candidates, target)
+print(ret)
