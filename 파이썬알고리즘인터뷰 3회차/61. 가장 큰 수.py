5e8df37d986271fa4a67430583f579e7672f5f0d
changhunJung <changhun611@gmail.com>
2023-08-17 23:54:18 +0900

    .

---
+++ 파이썬알고리즘인터뷰 3회차/61. 가장 큰 수.py
@@ -0,0 +1,52 @@
+from typing import List
+
+
+
+
+class Solution:
+    def largestNumber(self, nums: List[int]) -> str:
+        tmp = [0] * len(nums)
+
+        def is_ok(i, j):
+            return str(nums[i]) + str(nums[j]) >= str(nums[j]) + str(nums[i])
+
+        def mergeSort(s, e):
+            if s >= e:
+                return
+            m = (s + e) // 2
+            mergeSort(s, m)
+            mergeSort(m+1, e)
+
+            i = s
+            j = m + 1
+            k = s
+            while i <= m and j <= e:
+                if is_ok(i, j):
+                    tmp[k] = nums[i]
+                    k += 1
+                    i += 1
+                else:
+                    tmp[k] = nums[j]
+                    k += 1
+                    j += 1
+            while i <= m:
+                tmp[k] = nums[i]
+                k += 1
+                i += 1
+            while j <= e:
+                k += 1
+                j += 1
+
+            for k in range(s, e + 1):
+                nums[k] = tmp[k]
+
+        mergeSort(0, len(nums) -1 )
+        ans = ""
+        for num in nums:
+            ans += str(num)
+        return ans
+
+
+nums = [3,30,34,5,9]
+ret = Solution().largestNumber(nums)
+print(ret)
