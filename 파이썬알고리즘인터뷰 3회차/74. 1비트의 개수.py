36fe07e7e8fdaabf438ef045c0b7b86831ec11ca
changhunJung <changhun611@gmail.com>
2023-08-26 22:50:02 +0900

    .

--- 파이썬알고리즘인터뷰 3회차/74. 1비트의 개수.py
+++ 파이썬알고리즘인터뷰 3회차/74. 1비트의 개수.py
@@ -1,3 +1,7 @@
 class Solution:
     def hammingWeight(self, n: int) -> int:
-        return bin(n).count('1')
+        ans = 0
+        while n:
+            n &= (n-1)
+            ans += 1
+        return ans
