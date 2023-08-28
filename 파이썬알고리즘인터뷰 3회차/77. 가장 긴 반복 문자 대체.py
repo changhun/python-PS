730d6ac62aca3e17fdfc72579b931c6fd7a2578d
changhunJung <changhun611@gmail.com>
2023-08-29 00:04:19 +0900

    .

--- 파이썬알고리즘인터뷰 3회차/77. 가장 긴 반복 문자 대체.py
+++ 파이썬알고리즘인터뷰 3회차/77. 가장 긴 반복 문자 대체.py
@@ -11,11 +11,15 @@ class Solution:
 
             for i, ch in enumerate(s):
                 if ch != target_ch:
-                    if count == 0:
-                        st = q.popleft() + 1
-                        count += 1
-                    q.append(i)
-                    count -= 1
+                    if k != 0:
+                        if count == 0:
+                            st = q.popleft() + 1
+                            count += 1
+                        q.append(i)
+                        count -= 1
+                    else:
+                        st = i + 1
+                        continue
                 ans = max(ans, i - st + 1)
         return ans
 
@@ -24,6 +28,8 @@ k = 2
 s = "AABABBA"
 k = 1
 s = "AAAA"
+k = 0
+s = "ABAA"
 k = 0
 ret = Solution().characterReplacement(s,k)
 print(ret)
