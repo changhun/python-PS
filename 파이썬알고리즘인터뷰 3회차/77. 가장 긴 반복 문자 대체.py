cf1324e7b9daed7bee56219f2a1a3732e0e68a99
changhunJung <changhun611@gmail.com>
2023-08-28 23:54:28 +0900

    .

---
+++ 파이썬알고리즘인터뷰 3회차/77. 가장 긴 반복 문자 대체.py
@@ -0,0 +1,29 @@
+from collections import deque
+import string
+
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        ans = 0
+        for target_ch in list(string.ascii_uppercase):
+            count = k
+            st = 0
+            q = deque()
+
+            for i, ch in enumerate(s):
+                if ch != target_ch:
+                    if count == 0:
+                        st = q.popleft() + 1
+                        count += 1
+                    q.append(i)
+                    count -= 1
+                ans = max(ans, i - st + 1)
+        return ans
+
+s = "ABAB"
+k = 2
+s = "AABABBA"
+k = 1
+s = "AAAA"
+k = 0
+ret = Solution().characterReplacement(s,k)
+print(ret)
