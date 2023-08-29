e417084ba24d511866e3d03c7e537a1322f42369
changhunJung <changhun611@gmail.com>
2023-08-30 00:16:47 +0900

    completed

--- 파이썬알고리즘인터뷰 3회차/76. 부분 문자열이 포함된 최소 윈도우.py
+++ 파이썬알고리즘인터뷰 3회차/76. 부분 문자열이 포함된 최소 윈도우.py
@@ -1,9 +1,15 @@
 import collections
 INF = int(1e9)
 
+def is_all_not_bigger_then_zero(l):
+    for val in l:
+        if val > 0:
+            return False
+    return True
+
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        counts = collections.Counter(s)
+        counts = collections.Counter(t)
         st = 0
         min_win_st = 0
         min_win_end = INF
@@ -11,10 +17,25 @@ class Solution:
             ch = s[i]
             if ch in counts:
                 counts[ch] -= 1
-                if counts[ch] < 0:
-                    is_t_included = True
-                    for count in counts.values():
-                        if count > 0:
-                            is_t_included = False
+                if counts[ch] <= 0 and is_all_not_bigger_then_zero(counts.values()):
+                    while st < i:
+                        if s[st] in counts and counts[s[st]] == 0:
                             break
+                        if s[st] in counts:
+                            counts[s[st]] += 1
+                        st += 1
 
+                    if i - st + 1 < min_win_end - min_win_st + 1:
+                        min_win_end, min_win_st = i, st
+
+        if min_win_end == INF:
+            return ""
+        return s[min_win_st:min_win_end+1]
+
+
+s = "ADOBECODEBANC"
+t = "ABC"
+s = "a"
+t = "a"
+ret = Solution().minWindow(s, t)
+print(ret)
