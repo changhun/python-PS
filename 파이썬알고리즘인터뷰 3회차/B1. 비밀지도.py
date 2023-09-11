fce67437440195e5f2047dc0329f8ec57d139097
changhunJung <changhun611@gmail.com>
2023-09-11 23:32:01 +0900

    .

--- 파이썬알고리즘인터뷰 3회차/B1. 비밀지도.py
+++ 파이썬알고리즘인터뷰 3회차/B1. 비밀지도.py
@@ -2,18 +2,9 @@ def solution(n, arr1, arr2):
     answer = []
     or_value = []
     for i in range(n):
-        #b = bin(arr1[i]|arr2[i])
         oring = arr1[i] | arr2[i]
-
         s = []
-        """
-        for _ in range(n):
-            s.append(str(oring & 1))
-            oring >>= 1
-        s.reverse()
-        answer.append("".join(s))
-        # 문자열 치환으로 해보기
-        """
+
         for _ in range(n):
             if oring & 1:
                 s.append("#")
