025febdd291cdea26d803823b31a837ea3089762
changhunJung <changhun611@gmail.com>
2023-09-18 23:55:47 +0900

    4th

--- 파이썬알고리즘인터뷰 3회차/01. 유효한 팰린드롬 (leetcode 125).py
+++ 파이썬알고리즘인터뷰 3회차/01. 유효한 팰린드롬 (leetcode 125).py
@@ -1,20 +1,12 @@
-""" sol1"""
-"""
-class Solution:
-    def isPalindrome(self, s: str) -> bool:
-        l = [char.lower() for char in s if char.isalnum()]
-         return l == l[::-1]
-"""
-""" Sol 2 """
 import re
+
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        s = re.sub('[^a-zA-Z0-9]', '', s).lower()
-        return s == s[::-1]
+        t = re.sub("[^a-zA-Z0-9]", '', s).lower()
+        return t == t[::-1]
 
 
 
-s = "race a car"
 s = "A man, a plan, a canal: Panama"
 ret = Solution().isPalindrome(s)
 print(ret)
