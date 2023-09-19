83e4a1405e098a145b95773a6bdbc3ee65c8f09b
changhunJung <changhun611@gmail.com>
2023-09-19 22:25:49 +0900

    4th

--- 파이썬알고리즘인터뷰 3회차/03. 로그 파일 재정렬 (leetcodde 937).py
+++ 파이썬알고리즘인터뷰 3회차/03. 로그 파일 재정렬 (leetcodde 937).py
@@ -1,11 +1,6 @@
 class Solution:
     def reorderLogFiles(self, logs:list) -> list:
-        str_logs = [log for log in logs if log[-1].isalpha()]
+        char_logs = [log for log in logs if log[-1].isalpha()]
         num_logs = [log for log in logs if log[-1].isdigit()]
-        str_logs.sort(key = lambda x : (x.split()[1:], x.split()[0]))
-        ans = str_logs + num_logs
-        return ans
-
-logs = ["dig1 8 1 5 1","let1 art can","dig2 3 6","let2 own kit dig","let3 art zero"]
-ret = Solution().reorderLogFiles(logs)
-print(ret)
+        char_logs.sort(key=lambda x: (x.split()[1:], x.split()[0]))
+        return char_logs + num_logs
