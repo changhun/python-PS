da2a3fe92ac2a60790f8742f0a48c3e64be3b7c3
changhunJung <changhun611@gmail.com>
2023-09-20 23:48:18 +0900

    4th

--- 파이썬알고리즘인터뷰 3회차/04. 가장 흔한 단어 (leetcode 819).py
+++ 파이썬알고리즘인터뷰 3회차/04. 가장 흔한 단어 (leetcode 819).py
@@ -3,11 +3,10 @@ import collections
 
 class Solution:
     def mostCommonWord(self, paragraph: str, banned: list[str]) -> str:
-        words = list(re.sub('[^a-zA-Z]', ' ', paragraph).lower().split())
+        words = re.sub("[.,]", " ", paragraph).lower().split()
         words = [word for word in words if word not in banned]
-        print(words)
-        counter = collections.Counter(words)
-        return counter.most_common(1)[0][0]
+        return collections.Counter(words).most_common(1)[0][0]
+
 
 paragraph = "Bob hit a ball, the hit BALL flew far after it was hit."
 banned = ["hit"]
