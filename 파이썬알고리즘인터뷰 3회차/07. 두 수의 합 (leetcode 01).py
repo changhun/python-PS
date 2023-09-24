df9301de561667947495cfcd989ae08d1844f0ed
changhunJung <changhun611@gmail.com>
2023-09-25 00:35:09 +0900

    4th

--- 파이썬알고리즘인터뷰 3회차/07. 두 수의 합 (leetcode 01).py
+++ 파이썬알고리즘인터뷰 3회차/07. 두 수의 합 (leetcode 01).py
@@ -2,11 +2,17 @@ from typing import List
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        dic = {}
-        for i, num in enumerate(nums):
-            if target - num in dic:
-                return [i, dic[target - num]]
-            dic[num] = i
+        num_pairs = [(num, i) for i, num in enumerate(nums)]
+        num_pairs.sort()
+        left = 0
+        right = len(nums) - 1
+        while left < right:
+            if num_pairs[left][0] + num_pairs[right][0] == target:
+                return [num_pairs[left][1], num_pairs[right][1]]
+            elif num_pairs[left][0] + num_pairs[right][0] < target:
+                left += 1
+            else:
+                right -= 1
 
 
 nums = [2,7,11,15]
